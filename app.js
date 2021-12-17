63fbe31808acb89c178d775928c9fb19a0ce2f6b
eknarin lerdnuntawat <6110490023@student.tu.ac.th>
2021-12-18 02:24:15 +0700

    deployV.1

---
+++ app.js
@@ -0,0 +1,141 @@
+import line from '@line/bot-sdk';
+import express from 'express';
+import ngrok from 'ngrok';
+import axios from 'axios';
+import mqtt from 'mqtt';
+import dotenv from 'dotenv';
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs, doc, setDoc } from "firebase/firestore";
+// varible
+const port = process.env.PORT || 3000;
+let currentData = { "status": "undefine" };
+dotenv.config()
+
+var mqttClient = mqtt.connect(process.env.HIVEMQ_BROKER);
+//mqttClient
+mqttClient.on('connect', () => {
+     console.log('HIVEMQ connected');
+     mqttClient.subscribe(["cn466/door/cucumber_nut/#"], () => {
+          console.log("Topic subscribed");
+     });
+});
+
+mqttClient.on('message', (topic, payload) => {
+     console.log('Received Message:', topic, payload.toString())
+     currentData = JSON.parse(payload.toString());
+     notify(currentData.status)
+
+
+});
+//firebase
+
+const firebaseConfig = {
+     apiKey: "AIzaSyAjOywgHdA-wJP-ynu9zJvmQMJ62NBR9io",
+     authDomain: "cn466doorman.firebaseapp.com",
+     projectId: "cn466doorman",
+     storageBucket: "cn466doorman.appspot.com",
+     messagingSenderId: "698214544675",
+     appId: "1:698214544675:web:1a667f4ce0d8bae4898896",
+     measurementId: "G-J50BJPD46E"
+};
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore();
+async function set(nameCollection, nameDocument, data) {
+     try {
+          await setDoc(doc(db, nameCollection, nameDocument), data);
+
+          console.log("Document written with ID: ", nameDocument);
+     } catch (e) {
+          console.error("Error adding document: ", e);
+     }
+}
+async function get(nameCollection) {
+     const querySnapshot = await getDocs(collection(db, "users"));
+
+}
+
+const config = {
+     channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+     channelSecret: process.env.CHANNEL_SECRET,
+};
+
+const lineClient = new line.Client(config);
+const app = express();
+
+app.post('/callback', line.middleware(config), (req, res) => {
+     Promise
+          .all(req.body.events.map(handleEvent))
+          .then((result) => res.json(result))
+          .catch((err) => {
+               console.error(err);
+               res.status(500).end();
+          });
+});
+app.get('/test', async (req, res) => {
+     //เซตวันที่ส่งไป 12-12-2021 ว-ด-ปี
+     const querySnapshot = await getDocs(collection(db, "listDate"));
+     let listDate = []
+     querySnapshot.forEach((doc) => {
+          console.log(doc.id);
+          listDate.push(doc.id);
+     }
+     );
+     res.json({ data: listDate });
+});
+async function handleEvent(event) {
+     if (event.type !== 'message' || event.message.type !== 'text') {
+          // ignore non-text-message event
+          return Promise.resolve(null);
+     }
+     let text = event.message.text
+     if (text == 'สถานะ') {
+          let echo = [{ type: 'text', text: "สถานะประตู :"+currentData.status }];
+          return lineClient.replyMessage(event.replyToken, echo);
+     }
+     let arraytext = text.split(" ");
+     if (arraytext[1] == 'ดูประวัติ') {
+          const querySnapshot = await getDocs(collection(db,arraytext[0] ));
+          let dataDoc = "ประวัติของวันที่  " +arraytext[0]+"\n=======================";
+          querySnapshot.forEach((doc) => {
+               dataDoc = dataDoc+"\n" +doc.data().status+" ประตูเมื่อเวลา "+doc.data().Time ;
+          });
+          let echo = [{ type: 'text', text: dataDoc }];
+          return lineClient.replyMessage(event.replyToken, echo);
+     }
+     if (arraytext[1] == 'จำนวนครั้ง') {
+          const querySnapshot = await getDocs(collection(db,arraytext[0] ));
+          let count =0;
+          querySnapshot.forEach((doc) => {
+               count= count+1;
+          });
+          let dataDoc = "จำนวนการเปิด-ปิดประตู" +" เท่ากับ "+ count;
+          let echo = [{ type: 'text', text: dataDoc }];
+          return lineClient.replyMessage(event.replyToken, echo);
+     }
+
+     let echo = [];
+     echo.push({ type: 'text', text: text });
+     return lineClient.replyMessage(event.replyToken, echo);
+}
+
+function notify(status) {
+     var today = new Date();
+     var date = today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear();
+     var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+     set(date, time, { "status": "open", "Date": date, "Time": time });
+     set("listDate", date, {});
+     lineClient.broadcast({ type: 'text', text: "สถานะประตู: " + status });
+}
+
+async function initServices() {
+     //const baseURL = await ngrok.connect(port);
+     const baseURL = process.env.BASE_URL;
+     await lineClient.setWebhookEndpointUrl(baseURL + '/callback');
+     console.log('Set LINE webhook at ' + baseURL);
+}
+
+app.use(express.static('static'));
+initServices();
+app.listen(port, () => {
+     console.log(`listening on ${port}`);
+});
---
+++ impulse_data_forwarder/impulse_data_forwarder.ino
@@ -0,0 +1,70 @@
+#include <Wire.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_HTS221.h>
+#include <Adafruit_NeoPixel.h>
+#include <ESPAsync_WiFiManager.h>
+#include <PubSubClient.h>
+
+// constants
+#define I2C_SDA     41
+#define I2C_SCL     40
+#define LED_PIN     2
+#define RGBLED_PIN  18
+
+const char SENSOR_TOPIC[] = "cn466/sensors/cucumber_nut";
+const char LED_TOPIC[] = "cn466/leds/cucumber_nut";
+
+// persistence variables
+Adafruit_HTS221 hts;
+Adafruit_MPU6050 mpu;
+Adafruit_NeoPixel pixels(1, RGBLED_PIN, NEO_GRB + NEO_KHZ800);
+
+//AsyncWebServer webServer(80);
+//WiFiClient esp32Client;
+//DNSServer dnsServer;
+// setup sensors and LED
+
+void setupHardware() {
+  Wire.begin(I2C_SDA, I2C_SCL, 100000);
+  if (hts.begin_I2C()) {    // prepare HTS221 sensor
+    Serial.println("HTS221 sensor ready");
+  }
+  if (mpu.begin()) {       // prepare MPU6050 sensor
+    Serial.println("MPU6050 sensor ready");
+  }
+  pinMode(LED_PIN, OUTPUT);      // prepare LED
+  digitalWrite(LED_PIN, HIGH);
+  pixels.begin();                // prepare NeoPixel LED
+  pixels.clear();
+}
+
+// initialization
+void setup() {
+  Serial.begin(115200);
+  setupHardware();
+  Serial.println("Starting");
+}
+
+void loop() {
+  static uint32_t prev_millis = 0;
+  char json_body[200];
+  const char json_test[] = "%.2f,%.2f,%.2f";
+  sensors_event_t temp, humid;
+  sensors_event_t a, g;
+
+  if (millis() - prev_millis > 100) {
+    prev_millis = millis();
+    mpu.getEvent(&a, &g, &temp);
+    //float ax = a.acceleration.x;
+    //float ay = a.acceleration.y;
+    //float az = a.acceleration.z;
+    float gx = g.gyro.x;
+    float gy = g.gyro.y;
+    float gz = g.gyro.z;
+    //sprintf(json_body, json_tmpl, p, t, h, ax, ay, az, gx, gy, gz);
+    sprintf(json_body, json_test, gx, gy, gz);
+    Serial.println(json_body);
+  }
+   delay(100);
+  }
+ 
